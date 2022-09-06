af9abf58bd05cf21ba72cf82b6a15ecbd8d60223
Ronald Souza <telles.s@escolar.ifrn.edu.br>
2022-09-06 12:51:44 -0300

    banco de dados 100% funcionando, confia

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from core.models import Cliente, Tamanho, Pastel, Pedido, Bebida, BebidaPedida, PastelPedido
 from django.apps import apps
+
+# Register your models here.
+
 models = apps.get_models()
 for model in models:
     try:
         admin.site.register(model)
     except admin.sites.AlreadyRegistered:
         pass
-# Register your models here.
--- core/models.py
+++ core/models.py
@@ -7,20 +7,29 @@ class Cliente(models.Model):
   nome = models.CharField('Nome', max_length=255) 
   telefone = models.CharField('Telefone', max_length=255)
   endereco = models.CharField('Endereço', max_length=255)
+  def __str__(self):
+    return self.nome
 
 class Tamanho(models.Model):
   tamanho = models.CharField('Tamanho', max_length=255)
+  def __str__(self):
+      return self.tamanho
+  
 
 class Pastel(models.Model):
   sabor = models.CharField('Sabor', max_length=255)
   preco = models.FloatField('Preco', default=0)
+  def __str__(self):
+      return self.sabor
 
 class Bebida(models.Model):
   nomeBebida = models.CharField('NomeBebida', max_length=255) 
   preco = models.FloatField('Preco', default=0)
+  def __str__(self):
+      return self.nomeBebida
 
 class Pedido(models.Model):
-  idCliente = models.ForeignKey('Pedido', on_delete=models.CASCADE)
+  idCliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
   dataHora = models.DateTimeField('DataHora', auto_now=True)
 
 class PastelPedido(models.Model):
@@ -33,3 +42,5 @@ class BebidaPedida(models.Model):
   idPedido = models.ForeignKey('Pedido', on_delete=models.CASCADE)
   idBebida = models.ForeignKey('Bebida', on_delete=models.CASCADE) 
   quantia = models.IntegerField('Quantia', default=1) 
+  
+
