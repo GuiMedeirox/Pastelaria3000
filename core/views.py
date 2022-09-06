cec84a5c511a0db545f0e53cbc97273bdc0719b4
Guilherme Medeiros <50681750+GuiMedeirox@users.noreply.github.com>
2022-09-06 12:11:45 -0300

    novo migrate + db

--- core/views.py
+++ core/views.py
@@ -3,12 +3,13 @@ from core.models import Pastel, Bebida
 # Create your views here.
 
 def index(request):
+ 
   saborPastel = Pastel.objects.all()  
   bebida = Bebida.objects.all()
   context = {
     'saborPastel': saborPastel,
     'bebida': bebida
-  }
+  }  
   return render(request, 'index.html', context)
 
 def gerenciarPedidos(request): 
