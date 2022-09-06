8ac1828b7db86c4af0c37e50077061ecaf5b2399
Ronald Souza <telles.s@escolar.ifrn.edu.br>
2022-09-06 11:17:17 -0300

    Consertando erros nos models

--- core/models.py
+++ core/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-  idCliente = models.IntegerField(primary_key=True)
   nome = models.CharField('Nome', max_length=255) 
   telefone = models.CharField('Telefone', max_length=255)
   endereco = models.CharField('Endereço', max_length=255)
@@ -11,25 +10,23 @@ class Cliente(models.Model):
   def __str__(self):
     return self.nome
 
+class Sabor(models.Model):
+  nome = models.CharField(max_length=255)
 
 class Pastel(models.Model):
-  idPastel = models.IntegerField(primary_key=True)
-  sabor = models.CharField('Sabor', max_length=255, default=None)
+  sabor = models.ForeignKey(Sabor, on_delete=models.CASCADE)
   precoBase = models.FloatField()
   def __str__(self):
     return self.sabor
 
 
-
 class Pedido(models.Model):
-  idPedido = models.IntegerField(primary_key=True)
   idCliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
   dataHora = models.DateTimeField()
   def __str__(self):
     return self.idPedido
 
 class Bebida(models.Model):
-  idBebida = models.IntegerField(primary_key=True)
   nome = models.CharField('Nome', max_length=255)
   preco = models.FloatField()
   def __str__(self):
@@ -46,7 +43,6 @@ class BebidaPedida(models.Model):
     unique_together = ('idBebida','idPedido')  
 
 class PastelPedido(models.Model):
-  idPastelPedido = models.IntegerField(primary_key=True)
   idPedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
   idPastel = models.ForeignKey(Pastel, on_delete=models.CASCADE)
   quantidade = models.IntegerField()
